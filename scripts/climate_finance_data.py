ea425a8fc11116f0c25925a82d223a9f64aefd4c
Jorge Rivera <jorge.rivera@one.org>
2024-10-22 19:22:38 +0200

    combined data

--- scripts/climate_finance_data.py
+++ scripts/climate_finance_data.py
@@ -165,4 +165,5 @@ def export_available_donors_and_years(df: pd.DataFrame):
 if __name__ == "__main__":
     # Provider perspective data starts in 2012
     data = climate_finance_commitments(2009, 2022)
+    data.to_parquet(Paths.output / "climate_finance_provider_perspective_data.parquet")
     export_available_donors_and_years(data)
