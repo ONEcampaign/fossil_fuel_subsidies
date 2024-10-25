d26f186e04dc63deaf310c37cbeb2b984ef4a964
Luca Picci <lpicci96@gmail.com>
2024-10-25 13:22:23 +0200

    update ffs raw formatting

--- scripts/format_raw.py
+++ scripts/format_raw.py
@@ -17,44 +17,19 @@ CF = pd.read_parquet(
 
 def format_ffs_data() -> None:
     """Format the Fossil Fuel Subsidies data
-
-    This function formats the raw Fossil Fuel Subsidies data and saves it to the output folder.
-    Formatting operations
-    - Rename columns
-    - Add units column
-    - Convert value to numeric
-    - Add aggregates for mechanism, beneficiary, and fuel_type
-    - Add country_code column with converted country names
-    - Save the formatted data to the output folder
     """
 
-    (
-        FFS.rename(
-            columns={
-                "Country": "country_name",
-                "Year": "year",
-                "Fuel type": "fuel_type",
-                "Source": "source",
-                "USD, nominal": "value",
-                "Mechanism": "mechanism",
-                "Beneficiary": "beneficiary",
-            }
-        )
-        .assign(units="USD nominal")
-        .drop(columns="source")
-        .assign(
-            value=lambda d: pd.to_numeric(
-                d["value"].str.replace(r"\s+", "", regex=True)
-            )
-        )
-        .pipe(
-            add_aggregates,
-            agg_cols=["mechanism", "beneficiary", "fuel_type"],
-            id_cols=["country_name", "year", "units"],
-        )
-        .assign(country_code=lambda d: convert_entities(d.country_name))
-        .reset_index(drop=True)
-        .to_csv(Paths.output / "fossil_fuel_subsidies.csv", index=False)
+    (FFS
+      .rename(columns = {"Country": "country_name", "Year": "year", "USD, nominal": "value"})
+      .drop(columns = ['Source'])
+      .assign(value=lambda d: pd.to_numeric(d["value"].str.replace(r"\s+", "", regex=True)))
+      .dropna(subset='value')
+      .groupby(['country_name', 'year'])
+      .agg({'value': 'sum'})
+      .reset_index()
+      .assign(iso3_code=lambda d: convert_entities(d.country_name))
+      .pipe(add_income_level_column, 'iso3_code', id_type = 'ISO3')
+      .to_csv(Paths.output / "fossil_fuel_subsidies.csv", index=False)
     )
 
     logger.info("Fossil Fuel Subsidies data formatted and saved to output folder.")
@@ -62,14 +37,6 @@ def format_ffs_data() -> None:
 
 def format_cf_data() -> None:
     """Format the Climate Finance data
-
-    This function formats the raw Climate Finance data and saves it to the output folder.
-    Formatting operations
-    - Rename columns
-    - Drop methodology and flow_type columns
-    - Add aggregates for marker
-    - Add country_code column with converted country names
-    - Save the formatted data to the output folder
     """
 
     (CF
